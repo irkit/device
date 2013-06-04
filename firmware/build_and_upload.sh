aacb0e88b43e3093304de6a7eb717ae71b0f022c
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-06-04 18:47:45 +0900

    安心

--- firmware/build_and_upload.sh
+++ firmware/build_and_upload.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+set -xe
 rm -rf src/irkit/build-uno/
-~/src/ino/bin/ino build -m pro5v328 && ~/src/ino/bin/ino upload -m pro5v328
+~/src/ino/bin/ino clean
+~/src/ino/bin/ino build -m pro5v328
+~/src/ino/bin/ino upload -m pro5v328
