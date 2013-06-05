974a960b4c6d7e0ac8514fefd82cfd268d3db167
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-06-05 11:30:51 +0900

    faster build

--- firmware/build_and_upload.sh
+++ firmware/build_and_upload.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -xe
+
+# remove Arduino IDE build files
 rm -rf src/irkit/build-uno/
-~/src/ino/bin/ino clean
+
 ~/src/ino/bin/ino build -m pro5v328
 ~/src/ino/bin/ino upload -m pro5v328
