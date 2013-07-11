27ba8d0f1c55cfafbca37f90719444807ca52c89
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-07-11 11:33:15 +0900

    introduced WriteTemplate, which renders template and write to context

--- web/app.go
+++ web/app.go
@@ -3,14 +3,21 @@ package main
 import (
 	"github.com/hoisie/web"
 	"github.com/kayac/irkit/web/app"
+	"io"
 	"log"
 	"net/http/httputil"
 	"os"
 )
 const host = "0.0.0.0:9999"
 
-func hello(val string) string {
-	return "hello " + val
+func index(c *web.Context, val string) {
+	type data struct {
+		Name string
+	}
+	d := data{ val }
+
+	context := app.Context{*c}
+	context.WriteTemplate("templates/index.template", d)
 }
 
 func one(c *web.Context, val string) {
@@ -27,13 +34,14 @@ func one(c *web.Context, val string) {
 	}
 
 	context := app.Context{*c}
-	context.RenderTemplate("templates/one.template", d)
+	buf := context.RenderTemplate("templates/one.template", d)
+	io.Copy(c,buf)
 }
 
 func main() {
 	server := web.NewServer()
 	server.Get("/apps/one/(.*)", one)
-	server.Get("/(.*)",          hello)
+	server.Get("/(.*)",          index)
 	server.SetLogger( log.New(os.Stdout, "app ", log.Ldate|log.Ltime|log.Lshortfile ) )
 	server.Run(host)
 }
--- web/app/template.go
+++ web/app/template.go
@@ -11,21 +11,26 @@ type Context struct {
 	web.Context
 }
 
-func(c Context) RenderTemplate(path string, data interface{}) {
+func(c Context) RenderTemplate(path string, data interface{}) *bytes.Buffer {
 	buf := new(bytes.Buffer)
 
 	t, err := template.ParseFiles(path)
 	if err != nil {
 		c.Server.Logger.Println("err: " + err.Error())
 		c.Abort( 500, "Internal Server Error" )
-		return
+		return buf
 	}
 
 	err = t.ExecuteTemplate(buf, "T", data)
 	if err != nil {
 		c.Server.Logger.Println("err: " + err.Error())
 		c.Abort( 500, "Internal Server Error" )
-		return
+		return buf
 	}
-	io.Copy(c, buf)
+	return buf
+}
+
+func(c Context) WriteTemplate(path string, data interface{}) {
+	buf := c.RenderTemplate(path, data)
+	io.Copy(c,buf)
 }
