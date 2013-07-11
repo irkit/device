8ac731e4407e8c41366b22cd87660652c81d79d1
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-07-11 14:26:32 +0900

    gofmt

--- web/app.go
+++ web/app.go
@@ -8,25 +8,26 @@ import (
 	"os"
 	"strings"
 )
+
 const host = "0.0.0.0:9999"
 
 func index(c *web.Context, val string) {
 	type data struct {
 		Name string
 	}
-	d := data{ val }
+	d := data{val}
 
 	context := app.Context{*c}
 	context.WriteTemplate("templates/index.template", d)
 }
 
 func one(c *web.Context, val string) {
-	dump, _ := httputil.DumpRequest(c.Request,false)
+	dump, _ := httputil.DumpRequest(c.Request, false)
 	c.Server.Logger.Println(string(dump))
 
 	type data struct {
 		Title string
-		Host string
+		Host  string
 	}
 	d := data{
 		"<script>alert('you have been pwned')</script>",
@@ -35,14 +36,14 @@ func one(c *web.Context, val string) {
 
 	context := app.Context{*c}
 	buf := context.RenderTemplate("templates/one.template", d)
-	redirectTo := "data:text/html;charset=UTF-8," + strings.Replace( buf.String(), "\n", "", -1 )
-	c.Redirect( 302, redirectTo )
+	redirectTo := "data:text/html;charset=UTF-8," + strings.Replace(buf.String(), "\n", "", -1)
+	c.Redirect(302, redirectTo)
 }
 
 func main() {
 	server := web.NewServer()
 	server.Get("/apps/one/(.*)", one)
-	server.Get("/(.*)",          index)
-	server.SetLogger( log.New(os.Stdout, "app ", log.Ldate|log.Ltime|log.Lshortfile ) )
+	server.Get("/(.*)", index)
+	server.SetLogger(log.New(os.Stdout, "app ", log.Ldate|log.Ltime|log.Lshortfile))
 	server.Run(host)
 }
--- web/app/template.go
+++ web/app/template.go
@@ -11,26 +11,26 @@ type Context struct {
 	web.Context
 }
 
-func(c Context) RenderTemplate(path string, data interface{}) *bytes.Buffer {
+func (c Context) RenderTemplate(path string, data interface{}) *bytes.Buffer {
 	buf := new(bytes.Buffer)
 
 	t, err := template.ParseFiles(path)
 	if err != nil {
 		c.Server.Logger.Println("err: " + err.Error())
-		c.Abort( 500, "Internal Server Error" )
+		c.Abort(500, "Internal Server Error")
 		return buf
 	}
 
 	err = t.ExecuteTemplate(buf, "T", data)
 	if err != nil {
 		c.Server.Logger.Println("err: " + err.Error())
-		c.Abort( 500, "Internal Server Error" )
+		c.Abort(500, "Internal Server Error")
 		return buf
 	}
 	return buf
 }
 
-func(c Context) WriteTemplate(path string, data interface{}) {
+func (c Context) WriteTemplate(path string, data interface{}) {
 	buf := c.RenderTemplate(path, data)
-	io.Copy(c,buf)
+	io.Copy(c, buf)
 }
