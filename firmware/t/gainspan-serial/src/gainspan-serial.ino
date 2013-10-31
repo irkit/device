417ffa65aa12e6222b795784e1d19bde346085b4
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-10-31 21:43:27 +0900

    test raw UART with GS1011MIPS

--- firmware/t/gainspan-serial/src/gainspan-serial.ino
+++ firmware/t/gainspan-serial/src/gainspan-serial.ino
@@ -25,41 +25,10 @@ void loop() {
     // usb -> gainspan
     if (Serial.available()) {
         static uint8_t last_character = '0';
-        static bool isCommandMode = false;
 
         last_character = Serial.read();
 
-        if (isCommandMode) {
-            Serial1.write(last_character);
-
-            if ( last_character == 0x1B ) {
-                isCommandMode = false;
-                Serial.println(P("<< command mode finished !!!!"));
-            }
-            return;
-        }
-        else if (last_character == 0x1B) {
-            isCommandMode = true;
-            Serial.println(P(">> entered command mode !!!!"));
-            return;
-        }
-
+        Serial1.write(last_character);
         Serial.write(last_character);
-        Serial.println();
-
-        if (last_character == 'g') {
-            Serial1.end();
-            Serial1.begin(115200);
-
-            Serial.println("change Serial1 baud to 115200");
-        }
-        else if (last_character == 'G') {
-            Serial1.end();
-            Serial1.begin(9600);
-
-            Serial.println("change Serial1 baud to 9600");
-        }
-
-        Serial.print(P("> "));
     }
 }
