73ffc83025fd7845eb24f63e0f46d168180879e0
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-11-01 16:45:29 +0900

    version string changed to "#{MAJOR}.#{MINOR}.#{REVISION}.#{COMMITHASH}" something like: 1.2.0.71.g23c8f0d

--- firmware/build.sh
+++ firmware/build.sh
@@ -4,6 +4,6 @@ set -xe
 # remove Arduino IDE build files
 rm -rf src/main/build-uno/
 
-VERSION=`git describe --tags --exact-match || git rev-parse --short HEAD`
+VERSION=`git describe --tags --long | sed -e "s/^v//" | sed -e "s/\-/./g"`
 sed -e "s/__VERSION__/$VERSION/" src/main/version.template > src/main/version.c
 ~/src/ino/bin/ino build -m leonardo
