861731167aaf8caf44a37a27295be47d1dea646b
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-11-06 22:02:56 +0900

    use "irkit" model (boards.txt not included) to set vid,bid

--- firmware/build.sh
+++ firmware/build.sh
@@ -6,4 +6,4 @@ rm -rf src/main/build-uno/
 
 VERSION=`git describe --tags --long | sed -e "s/^v//" | sed -e "s/\-/./g"`
 sed -e "s/__VERSION__/$VERSION/" src/main/version.template > src/main/version.c
-~/src/ino/bin/ino build -m leonardo
+~/src/ino/bin/ino build -m irkit
--- firmware/upload.sh
+++ firmware/upload.sh
@@ -2,6 +2,6 @@
 set -xe
 
 # don't want to write boot loader area of leonardo
-ruby ./check_flash_size.rb ".build/leonardo/firmware.hex" 28672
+ruby ./check_flash_size.rb ".build/irkit/firmware.hex" 28672
 
-~/src/ino/bin/ino upload -m leonardo
+~/src/ino/bin/ino upload -m irkit
