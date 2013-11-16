5d23217b627bf3594a4fb821ea2458fe4991d099
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-11-16 22:18:16 +0900

    use accurate datasize by avr-size

--- firmware/upload.sh
+++ firmware/upload.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-set -xe
+set -e
 
 # don't want to write boot loader area of leonardo
-ruby ./check_flash_size.rb ".build/irkit/firmware.hex" 28672
+AVRBIN=/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin
+DATASIZE=`$AVRBIN/avr-size -B .build/irkit/firmware.hex | tail -n 1 | cut -b 11-15`
+if [ "$DATASIZE" -gt 28672 ]; then
+    echo "size is $DATASIZE > 28672, NG"
+    exit 1
+else
+    echo "size is $DATASIZE < 28672, OK"
+fi
 
 ~/src/ino/bin/ino upload -m irkit
