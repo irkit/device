45819511e18a838cbda8ef0bb0e3a95a1f5628d5
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-12-25 13:22:45 +0900

    packer-arduino just saves tree

--- firmware/t/packer-arduino/src/packer.ino
+++ firmware/t/packer-arduino/src/packer.ino
@@ -4,7 +4,7 @@
 void setup() {
     Serial.begin(115200);
 
-    // while ( ! Serial ) ; // wait for leonardo
+    while ( ! Serial ) ; // wait for leonardo
 
     int8_t result = irpacker_save( (void*) 169 );
     if (result == 0) {
