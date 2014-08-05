4d2c425b6d10fcefaa8442d54b53293ab0812f09
Masakazu OHTSUKA <o.masakazu@gmail.com>
2014-08-05 15:43:11 +0900

    Save map files, nm results by default

--- firmware/build.sh
+++ firmware/build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -xe
 
+AVRBIN=/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin
+
 # remove Arduino IDE build files
 rm -rf src/IRKit/build-uno/
 
 VERSION=`git describe --tags --long | sed -e "s/^v//" | sed -e "s/\-/./g"`
 sed -e "s/__VERSION__/$VERSION/" src/IRKit/version.template > src/IRKit/version.c
-~/src/ino/bin/ino build -m irkit
+~/src/ino/bin/ino build -m irkit --ldflags="-Os --gc-sections -Map=.build/irkit/firmware.map"
+${AVRBIN}/avr-nm --size-sort .build/irkit/firmware.elf > .build/irkit/firmware.nm.size.log
+${AVRBIN}/avr-nm --numeric-sort .build/irkit/firmware.elf > .build/irkit/firmware.nm.numeric.log
